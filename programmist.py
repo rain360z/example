2583b2c146aac741ab4f4ffd2bf5cda0726dfeb6
a.mitrofanov <a.mitrofanov@guso.loc>
2021-02-20 14:42:48 +0400

    scr create file with count programmist

--- programmist.py
+++ programmist.py
@@ -6,24 +6,24 @@ while i < 1000
 	f = open('resault.txt', 'w')
 	if acount == 0 or acount == 5 or acount == 6 \
 	or acount == 7 or acount == 8 or acount == 9:
-		print(count, 'программистов')
+		f.write(count, 'программистов', \n)
 	elif acount == 1:
 		if ccount == 1:
-			print(count, 'программист')
+			f.write(count, 'программист', \n)
 		elif int(count[-1]) == int(count[-2]):
-			print(count, 'программистов')
+			f.write(count, 'программистов', \n)
 		else:
-			print(count, 'программист')
+			f.write(count, 'программист', \n)
 	else:
 		if ccount == 2 or ccount == 3 or ccount == 4:
-			print(count, 'программиста')
+			f.write(count, 'программиста', \n)
 		elif int(count[-1]) + int(count[-2]) == 2 \
 		or int(count[-1]) + int(count[-2]) == 3 \
 		or int(count[-1]) + int(count[-2]) == 4 \
 		or int(count[-1]) + int(count[-2]) == 5 :
-			print(count, 'программистов')
+			f.write(count, 'программистов', \n)
 		else:
-			print(count, 'программиста')
+			f.write(count, 'программиста', \n)
 	i += 1
 
 
